82440d30afa4c48f87aab3e99123c5df04ddf195
Saksham <sakshamsutar.1@gmail.com>
2026-04-26 14:33:14 +0530

    Streamlit setup + Homescreen

---
+++ src/components/footer.py
@@ -0,0 +1,10 @@
+import streamlit as st 
+
+
+
+def footer_home():   
+    st.markdown(f"""
+            <div style='margin-top:2rem; text-align:center;'>
+             <p style='font-weight:bold'>Created By Saksham</p>
+            </div>
+            """,unsafe_allow_html=True)
