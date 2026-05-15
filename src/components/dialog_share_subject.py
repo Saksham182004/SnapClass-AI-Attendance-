9c2072400dd6e20a8312ca194ca4a18d321c6cc6
Saksham <sakshamsutar.1@gmail.com>
2026-05-16 02:57:12 +0530

    changing app domain

--- src/components/dialog_share_subject.py
+++ src/components/dialog_share_subject.py
@@ -4,7 +4,7 @@ import io
 
 @st.dialog("Share class Link")
 def share_subject_dialog(subject_name, subject_code):
-    app_domain = "http://localhost:8501"
+    app_domain = "snapclass-main.streamlit.app"
     join_url = f"{app_domain}/?join-code={subject_code}"
 
     qr = segno.make(join_url)
